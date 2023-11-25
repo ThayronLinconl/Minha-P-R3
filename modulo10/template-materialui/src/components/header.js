6477b11402fc126a04aea3800ec273d2ba7a1be4
Thayron Lincoln <thayronlinconl@gmail.com>
2023-11-25 15:05:55 -0300

    Mudança de codigo

--- modulo10/template-materialui/src/components/header.js
+++ modulo10/template-materialui/src/components/header.js
@@ -5,31 +5,35 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
-//import MenuIcon from '@mui/icons-material/Menu';
- 
+
 function Header() {
     return (
         <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{ mr: 2 }}
-            >
-              {/*  
-              <MenuIcon />
-              */}
-            </IconButton>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              Gerenciamento de Projetos
-            </Typography>
-          </Toolbar>
-        </AppBar>
-      </Box>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        sx={{ mr: 4 }}
+                    />
+                    <Typography 
+                        variant="h1" 
+                        component="div" 
+                        sx={{
+                            flexGrow: 1,
+                            fontSize: '3rem', // Altera o tamanho da fonte
+                            fontFamily: 'Arial, sans-serif', // Altera a fonte
+                            fontWeight: 'bold', // Define o peso da fonte como bold
+                        }}
+                    >
+                        Projeto
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        </Box>
     );
 }
- 
+
 export default Header;
